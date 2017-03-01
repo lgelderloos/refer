f953c20bffc398d8cf9060b385007bd93fab08a7
lgelderloos <liekegelderloos@gmail.com>
2017-03-01 10:08:49 +0100

    extract VGG16 fc2

--- extract.py
+++ extract.py
@@ -4,8 +4,13 @@ from keras.applications.vgg16 import preprocess_input
 from keras.models import Model
 import numpy as np
 import cPickle as pickle
+import json
 
 base_model = VGG16(weights='imagenet')
+# note: this is using the pre-softmax layer form the 16 layer VGGNet.
+# not sure if Lazaridou et al a0 use 16 or 19 layer VGGNet and b)
+# use this exact layer ("second to last fully connected layer" could
+# also mean fc1?)
 model = Model(input=base_model.input, output=base_model.get_layer('fc2').output)
 cropids = []
 
@@ -14,10 +19,12 @@ with open("boxes.json", "r") as f:
     
 for img in boxes.keys():
     for obj in boxes[img]['objects']:
-        cropids.append((img, obj))
-print len(cropids)
-"""
-features = numpy.zeros(len(cropnames), 4096)
+        cropids.append( (img, obj) )
+
+print "nr of pairs: " + str(len(cropids))
+
+features = np.zeros( (len(cropids), 4096) )
+
 for i in range(len(cropids)):
     img_path = "data/images/crops/{}_{}.jpg".format(cropids[i][0], cropids[i][1])
     img = image.load_img(img_path, target_size=(224, 224))
@@ -26,13 +33,15 @@ for i in range(len(cropids)):
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
-    features[i,:] = model.predict(x)
+    feats = model.predict(x)
+#    print feats.shape
+#    print feats[0:10]
+    features[i,:] = feats[0,:]
     
 # save feature array
-np.save("data/crops/VGGfeats.npy", feats)
+np.save("data/crops/VGG16_fc2.npy", features)
     
 # dump list with (img, obj) tuples
 # (same order as numpy array)
 with open("crop_ids.p", wb) as f:
-    pickle.dump 
-"""
+    pickle.dump(cropids, f)
